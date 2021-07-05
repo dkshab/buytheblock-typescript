cdeddd179f7fc5868a40aecd27d2846d9146c5ed
Dumisani <dumisanik.shabangu@gmail.com>
2021-07-05 18:58:00 +0200

    Added background images on the Home Page

--- src/apps/NonAuthApp/NonAuthApp.tsx
+++ src/apps/NonAuthApp/NonAuthApp.tsx
@@ -1,11 +1,13 @@
-import HomeNonAuth from './static/HomeNonAuth/HomeNonAuth';
-import NavBarNonAuth from './static/NavBarNonAuth/NavBarNonAuth';
+import FooterNonAuth from "./static/FooterNonAuth/FooterNonAuth";
+import HomeNonAuth from "./static/HomeNonAuth/HomeNonAuth";
+import NavBarNonAuth from "./static/NavBarNonAuth/NavBarNonAuth";
 
 const NonAuthApp = () => {
   return (
     <div className="NonAuthApp">
       <NavBarNonAuth />
       <HomeNonAuth />
+      <FooterNonAuth />
     </div>
   );
 };
---
+++ src/apps/NonAuthApp/static/FooterNonAuth/FooterNonAuth.jsx
@@ -0,0 +1,9 @@
+const FooterNonAuth = () => {
+  return (
+    <div className="FooterNonAuth">
+      <p>© 2021 Buy The Block. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default FooterNonAuth;
